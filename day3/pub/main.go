c566320864a84053329d5e32c39b19e2caa6adb1
iamjonn <jonata10.jn@gmail.com>
2024-03-23 13:55:45 -0300

    feito comentarios para melhor memorizaçao

--- day3/pub/main.go
+++ day3/pub/main.go
@@ -8,35 +8,35 @@ import (
 ) // bliblioteca do google cloud pubsub
 
 func main() {
-	ctx := context.Background()
+	ctx := context.Background() // ctx é o contexto da aplicação
 
-	projectid := flag.String("projectid", "", "project id")
-	topicNAme := flag.String("topic", "", "Nome do tópico")
-	msg := flag.String("msg", "", "Mensagem a ser enviada")
-	flag.Parse()
+	projectid := flag.String("projectid", "", "project id") // flag para pegar o id do projeto
+	topicNAme := flag.String("topic", "", "Nome do tópico") // flag para pegar o nome do tópico
+	msg := flag.String("msg", "", "Mensagem a ser enviada") // flag para pegar a mensagem a ser enviada
+	flag.Parse() // parse das flags que serve para pegar os valores das flags
 
-	c, err := pubsub.NewClient(ctx, *projectid)
+	c, err := pubsub.NewClient(ctx, *projectid) // c é o cliente do pubsub
 	if err != nil {
-		panic(err)
+		panic(err) // se der erro, o programa para
 	}
-	defer c.Close()
+	defer c.Close() // fecha o cliente
 
-	topic := c.Topic(*topicNAme)
-	exist, err := topic.Exists(ctx)
+	topic := c.Topic(*topicNAme) // topic é o tópico
+	exist, err := topic.Exists(ctx) // exist é uma variável que verifica se o tópico existe
 	if err != nil {
-		panic(err)
+		panic(err) // se der erro, o programa para
 	}
 	if !exist {
-		topic, err = c.CreateTopic(ctx, *topicNAme)
+		topic, err = c.CreateTopic(ctx, *topicNAme) // cria o tópico
 		if err != nil {
-			panic(err)
+			panic(err) // se der erro, o programa para
 		}
 	}
-	result := topic.Publish(ctx, &pubsub.Message{
+	result := topic.Publish(ctx, &pubsub.Message{ // publica a mensagem no tópico
 		Data: []byte(*msg),
 	})
 
-	_, err = result.Get(ctx)
+	_, err = result.Get(ctx) // pega o resultado e verifica se deu erro
 	if err != nil {
 		panic(err)
 	}
